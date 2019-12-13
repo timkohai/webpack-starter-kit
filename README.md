b6fb62100b1f025fb40c89234fdb2e65a5ba666a
Timothy Madriaga <timothy.madriaga@cheetahdigital.com>
2019-12-13 14:03:22 +0800

    Initialize

---
+++ README.md
@@ -0,0 +1,12 @@
+# Webpack starter kit
+
+> Baseline configuration for Webpack
+
+Takes HTML, CSS, and JS files (`src/`) and compiles them (`build/`) using [Webpack]. This is the [webpack.config.js] that I usually start with in new projects.
+
+
+## What's in this project?
+
+### Directories
+There are four main directories in project. This directories like below:
+
---
+++ build/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path') 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        // filename: '[name].[chunkhash].js',
+        filename: '[name].js',
+        path: path.join(__dirname, 'build')
+    },
+    devServer: {
+        host: '0.0.0.0',
+        contentBase: path.join(__dirname, 'build'),
+        port: 9005
+
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: "babel-loader"
+            }
+        }]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({template: './src/template/index.html'})
+            
+    ]
+
+};
